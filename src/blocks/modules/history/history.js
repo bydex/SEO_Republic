932d2c2108d62d08ca79d570a50b3007a9dcf7ce
NickolasDzR <nikolyae2019@gmail.com>
2020-11-25 20:44:18 +0300

    some new updates

--- src/blocks/modules/history/history.js
+++ src/blocks/modules/history/history.js
@@ -1,44 +1,44 @@
-import slick from 'slick-carousel';
+// import slick from 'slick-carousel';
 
-$(document).ready(() => {
-    let slider = $(".history__items"),
-    hasSlider = undefined;
+// $(document).ready(() => {
+//     let slider = $(".history__items"),
+//     hasSlider = undefined;
 
-    function initSlider() {
-        if ($(window).width() <= 576 && !hasSlider) {
-            hasSlider = true;
+//     function initSlider() {
+//         if ($(window).width() <= 576 && !hasSlider) {
+//             hasSlider = true;
 
-            slider.slick({
-              infinite: false,
-              arrows: false,
-              responsive: [
-                {
-                  breakpoint: 380,
-                  settings: {
-                    slidesToShow: 1,
-                  },
-                },
-                {
-                  breakpoint: 420,
-                  settings: {
-                    slidesToShow: 1.1,
-                  },
-                },
-                {
-                  breakpoint: 2000,
-                  settings: {
-                    slidesToShow: 1.4,
-                  },
-                },
-              ],
-            });
-        } else if ($(window).width() > 576 && hasSlider) {
-            hasSlider = undefined;
-            slider.slick("unslick");
-        }
-    }
-    initSlider();
+//             slider.slick({
+//               infinite: false,
+//               arrows: false,
+//               responsive: [
+//                 {
+//                   breakpoint: 380,
+//                   settings: {
+//                     slidesToShow: 1,
+//                   },
+//                 },
+//                 {
+//                   breakpoint: 420,
+//                   settings: {
+//                     slidesToShow: 1.1,
+//                   },
+//                 },
+//                 {
+//                   breakpoint: 2000,
+//                   settings: {
+//                     slidesToShow: 1.4,
+//                   },
+//                 },
+//               ],
+//             });
+//         } else if ($(window).width() > 576 && hasSlider) {
+//             hasSlider = undefined;
+//             slider.slick("unslick");
+//         }
+//     }
+//     initSlider();
 
-    $(window).resize(initSlider);
+//     $(window).resize(initSlider);
 
-})
+// })
